82936fd98a0ea4ef16e5bbd83f1098ef1dc5d3c0
Pham-Van-Khiem-Toan-Tin <phamkhiemhust2001@gmail.com>
2025-03-27 02:23:12 +0700

    Initial commit

---
+++ backend/controllers/orderController.js
@@ -0,0 +1,204 @@
+const asyncHandler = require("express-async-handler");
+const { Order } = require("../models/orderModel");
+const { log } = require("console");
+let $ = require("jquery");
+const request = require("request");
+const moment = require("moment");
+const crypto = require("crypto");
+function sortParams(obj) {
+  const sortedObj = Object.entries(obj)
+    .filter(
+      ([key, value]) => value !== "" && value !== undefined && value !== null
+    )
+    .sort(([key1], [key2]) => key1.toString().localeCompare(key2.toString()))
+    .reduce((acc, [key, value]) => {
+      acc[key] = value;
+      return acc;
+    }, {});
+
+  return sortedObj;
+}
+// @desc    Create new order
+// @route   POST /api/orders
+// @access  Private
+const addOrderItems = asyncHandler(async (req, res) => {
+  const { orderItems, shippingAddress, paymentMethod } = req.body;
+
+  if (orderItems && orderItems.length === 0) {
+    res.status(400);
+    throw new Error("No order items");
+    return;
+  } else {
+    let itemsPrice = orderItems.reduce(
+      (acc, item) => acc + item.price * item.qty,
+      0
+    );
+    let taxPrice = 0.15 * itemsPrice;
+    let shippingPrice = 0;
+    let totalPrice = itemsPrice + taxPrice + shippingPrice;
+    const order = new Order({
+      orderItems,
+      user: req.user._id,
+      shippingAddress,
+      paymentMethod,
+      itemsPrice,
+      taxPrice,
+      shippingPrice,
+      totalPrice,
+    });
+
+    const createdOrder = await order.save();
+    process.env.TZ = "Asia/Ho_Chi_Minh";
+
+    let date = new Date();
+    let createDate = moment(date).format("YYYYMMDDHHmmss");
+
+    let ipAddr =
+      req.headers["x-forwarded-for"] ||
+      req.connection.remoteAddress ||
+      req.socket.remoteAddress ||
+      req.connection.socket.remoteAddress;
+
+    let config = require("config");
+
+    let tmnCode = config.get("vnp_TmnCode");
+    let secretKey = config.get("vnp_HashSecret");
+    let vnpUrl = config.get("vnp_Url");
+    let returnUrl = config.get("vnp_ReturnUrl");
+    let orderId = createdOrder._id.toString();
+    let amount = totalPrice;
+    let bankCode = null;
+
+    let locale = "vn";
+    let currCode = "VND";
+    let vnp_Params = {};
+    vnp_Params["vnp_Version"] = "2.1.0";
+    vnp_Params["vnp_Command"] = "pay";
+    vnp_Params["vnp_TmnCode"] = tmnCode;
+    vnp_Params["vnp_Locale"] = locale;
+    vnp_Params["vnp_CurrCode"] = currCode;
+    vnp_Params["vnp_TxnRef"] = orderId;
+    vnp_Params["vnp_OrderInfo"] = "Thanh toan cho ma GD:" + orderId;
+    vnp_Params["vnp_OrderType"] = "other";
+    vnp_Params["vnp_Amount"] = amount * 100;
+    vnp_Params["vnp_ReturnUrl"] = returnUrl;
+    vnp_Params["vnp_IpAddr"] = ipAddr;
+    vnp_Params["vnp_CreateDate"] = createDate;
+    if (bankCode !== null && bankCode !== "") {
+      vnp_Params["vnp_BankCode"] = bankCode;
+    }
+
+    // vnp_Params = sortObject(vnp_Params);
+
+    // let querystring = require("qs");
+    // let signData = querystring.stringify(vnp_Params, { encode: false });
+    // console.log({ signData });
+
+    // let hmac = crypto.createHmac("sha512", secretKey);
+    // let signed = hmac.update(Buffer.from(signData, "utf-8")).digest("hex");
+    // vnp_Params["vnp_SecureHash"] = signed;
+    // vnpUrl += "?" + querystring.stringify(vnp_Params, { encode: false });
+    // console.log(vnpUrl);
+    const sortedParams = sortParams(vnp_Params);
+    const urlParams = new URLSearchParams();
+    for (let [key, value] of Object.entries(sortedParams)) {
+      urlParams.append(key, value);
+    }
+    const querystring = urlParams.toString();
+    const hmac = crypto.createHmac("sha512", secretKey);
+    const signed = hmac.update(Buffer.from(querystring.toString(), "utf-8")).digest("hex");
+    urlParams.append("vnp_SecureHash", signed);
+    const paymentUrl = `${vnpUrl}?${urlParams.toString()}`;
+    console.log(paymentUrl);
+    res.status(200).json({ url: paymentUrl });
+    
+  }
+});
+
+// @desc    Get order by ID
+// @route   POST /api/orders/:id
+// @access  Private
+const getOrderById = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id).populate(
+    "user",
+    "name email"
+  );
+
+  if (order) {
+    res.json(order);
+  } else {
+    res.status(404);
+    throw new Error("Order not found");
+  }
+});
+
+// @desc    Update order to paid
+// @route   GET /api/orders/:id/pay
+// @access  Private
+const updateOrderToPaid = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id);
+
+  if (order) {
+    order.isPaid = true;
+    order.paidAt = Date.now();
+    order.paymentResult = {
+      id: req.body.id,
+      status: req.body.status,
+      update_time: req.body.update_time,
+      email_address: req.body.payer.email_address,
+    };
+
+    const updatedOrder = await order.save();
+
+    res.json(updatedOrder);
+  } else {
+    res.status(404);
+    throw new Error("Order not found");
+  }
+});
+
+// @desc    Update order to delivered
+// @route   POST /api/orders/:id/deliver
+// @access  Private
+const updateOrderToDelivered = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id);
+
+  if (order) {
+    order.isDelivered = true;
+    order.deliveredAt = Date.now();
+
+    const updatedOrder = await order.save();
+
+    res.json(updatedOrder);
+  } else {
+    res.status(404);
+    throw new Error("Order not found");
+  }
+});
+
+// @desc    Get logged in user orders
+// @route   GET /api/orders/myorders
+// @access  Private
+const getMyOrder = asyncHandler(async (req, res) => {
+  const orders = await Order.find({ user: req.user._id });
+
+  res.json(orders);
+});
+
+// @desc    Get all orders
+// @route   GET /api/orders
+// @access  Private/Admin
+const getOrders = asyncHandler(async (req, res) => {
+  const orders = await Order.find({}).populate("user", "id name");
+
+  res.json(orders);
+});
+
+module.exports = {
+  addOrderItems,
+  getOrderById,
+  updateOrderToPaid,
+  updateOrderToDelivered,
+  getMyOrder,
+  getOrders,
+};
